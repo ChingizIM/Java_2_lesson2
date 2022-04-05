4863169eff0da2ba0fafddf919c1421fc1c1c4cd
ChingizIM <funfulik.83@gmail.com>
2022-04-05 12:03:50 +0600

    Rectangle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	Figure[] figure = {
             new Square(10),
             new Square(23),
-    new Rectangle(7,8),
+        new Rectangle(7,8),
         new Rectangle(8,9),
         new Rectangle(13,58)};
         for (Figure value : figure) {
